2c657c5ea361ffbf48340c4ec7c58b0b0fab0376
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2023-02-15 22:14:24 +0200

    timer created

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+    // Tabs
 
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
@@ -35,7 +36,85 @@ window.addEventListener('DOMContentLoaded', () => {
                     hideTabContent();
                     showTabContent(i);
                 }
-            })
+            });
         }
-    })
+    });
+
+
+    // Timer
+
+    const deadline = '2023-03-11'; // задаем дедлайн
+
+    function getTimeRemaining(endtime) {
+        // в качестве аргумента будет дедлайн
+        const t = Date.parse(endtime) - Date.parse(new Date()); // => техническая переменная; получаем разницу между дедлайном и текущим временем в миллисекундах
+
+        // теперь эту разницу превращаем в кол-во дней, часов, минут и секунд
+        // 1) получаем дни, кол-во м/c делим на кол-во м/с в одном дне и округляем с помощью Math.floor()
+        const days = Math.floor(t / (1000 * 60 * 60 * 24));
+        // 1000 * 60 => м/с в одной минуте, * 60 => в одном часе, * 24 => в сутках
+        const hours = Math.floor((t / (1000 * 60 * 60) % 24)); // не больше чем 24 
+        // t / (1000 * 60 * 60) => получаем кол-во часов, но их может быть больше 24, поэтому получим кол-во целых суток и ОСТАТОК от них
+        const minutes = Math.floor((t / 1000 / 60) % 60); // => не больше чем 60
+        const seconds = Math.floor((t / 1000) % 60);
+
+        // тк эти переменные пока существуют пока только внутри этой функции, возвращаем их наружу в виде объекта
+
+        return {
+            'total': t, // общее кол-во миллисекунд
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    // добавляем нолики к однозначным числам
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    // теперь функция которая устанавливает таймер на страничку
+
+    function setClock(selector, endtime) {
+        // в качестве атрибутов главный элемент с таймером со страницы и дедлайн
+        // сначала получаем элементы со страницы
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000); // => а здесь запускаем нашу функцию каждую секунду, тк время обновляется каждую секунду
+
+              updateClock(); // это чтобы не ждать 1000 миллисекунд чтобы не было мигания
+
+        // теперь третья функция, которая будет обновлять наш таймер каждую секунду
+
+        function updateClock() {
+            // 1) расчет времени которое осталось на текущую секунду
+            const t = getTimeRemaining(endtime);
+
+            // 2) расчетные величины помещаем на страницу
+            // из t => из нашего технического объекта с днями, часами, минутами и секундами
+             days.innerHTML = getZero(t.days);
+             hours.innerHTML = getZero(t.hours);
+             minutes.innerHTML = getZero(t.minutes);
+             seconds.innerHTML = getZero(t.seconds);
+
+
+             // теперь останавливаем наш таймер
+             if (t.total <= 0) {
+                clearInterval(timeInterval); // => когда наш дедлайн прошел и текущая дата будет больше чем дедлайн
+             }
+        }
+    }
+    
+    setClock('.timer', deadline);
+
+    // теперь исправляем мигание и добавляем нолики к однозначному числу (09 часов etc)
 });
